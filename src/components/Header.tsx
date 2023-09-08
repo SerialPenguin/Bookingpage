45f3859c72bb6f525bdfd43737390a391aec98ff
Anna Krifors <krifors@hotmail.com>
2023-09-08 13:44:02 +0200

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,13 @@
 import "../stylesheet/components/_Header.scss"
+
 function Header():JSX.Element{
 return(
     <header>
+        <div className="header-wrapper">
+        <img src="/assets/hantel.jpeg" alt="Hantel" />
+
         <h1>Strong n' Epic</h1>
+        </div>
     </header>
 )
 }
